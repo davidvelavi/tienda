22e614126029509edb85531bdd5febf1ab7dd8b9
David Alejandro Velazquez Villegas <DavidAlejandro@C02VC4KPG8WN.local>
2019-12-10 16:48:43 -0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
+import Header from '../src/components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <HomePage/>
     </div>
   );
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.styles.scss';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+
+
+const Header = ()=>(
+  <div className="Header">
+    <Logo className="Header-Logo" />
+    <div className="Header-Options">
+      <div className="Header-Options">
+        <span className="Header-Option"> Shop </span>
+        <span className="Header-Option"> Contact </span>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
