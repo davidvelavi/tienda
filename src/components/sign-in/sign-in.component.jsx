55e2ea3bb001100cab4f5c011e0f9ba8a42f03df
David Alejandro Velazquez Villegas <DavidAlejandro@C02VC4KPG8WN.local>
2019-12-16 13:14:47 -0600

    google authentication with firebase added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../cutom-button/custom-button.component';
+import {signInWithGoogle} from '../../firebase/firebase.utilis';
+
 class  SignIn extends React.Component{
 
   constructor(props){
@@ -52,7 +54,7 @@ class  SignIn extends React.Component{
           />
           <div className="SignIn-Buttons">
             <CustomButton type="submit"> Sign In  </CustomButton>
-            <CustomButton isGoogleSignIn>  Sign in with Google </CustomButton>
+            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>  Sign in with Google </CustomButton>
           </div>
         </form>
       </div>
